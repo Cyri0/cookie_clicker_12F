ab0f58eb13e118cab6306840ca61f35973daf21e
Nemes Tamás <nemestamas94@gmail.com>
2025-04-04 12:19:27 +0200

    base works poorly 💩

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,10 @@ const App = () => {
   const [cookie, setCookie] = useState(0)
   const [items, setItems] = useState<ItemType[]>([
     {icon: "👵", cookiePerSec: 10, price: 5},
-    {icon: "🏭", cookiePerSec: 100, price: 10},
+    {icon: "🏭", cookiePerSec: 100, price: 500},
   ])
+
+  const [pocket, setPocket] = useState<ItemType[]>([])
 
   const click = () => {
     setCookie(cookie + 1)
@@ -24,17 +26,34 @@ const App = () => {
     }
   }, [])
 
+  const buyItem = (item: ItemType) => {
+    setCookie(prev => prev - item.price)
+    setPocket(prev => [...prev, item])
+  }
+  
+  useEffect(()=>{
+    const intervalId = setInterval(()=>{
+      pocket.forEach(item => {
+        setCookie(prev => {
+          localStorage.setItem("cookie", `${prev}`)          
+          return prev + (item.cookiePerSec / 10)})
+      })
+    },100)
+
+    return () => clearInterval(intervalId)
+  }, [pocket])
+
   return (
     <div>
       <p>{cookie}</p>
       <button onClick={click}>🍪</button>
-
       {
         items.map(item =>
-        <button disabled={cookie < item.price}>
+        <button disabled={cookie < item.price} onClick={() => buyItem(item)}>
           {item.icon}
         </button>) 
       }
+      <p>Zseb: { pocket.map(item => <span>{item.icon}</span>) }</p>
     </div>
    )
 }
